06cf15c5bfc0027b151f5bbbca8abd48f3390986
Zak Stamps <theoneandonlyzako@gmail.com>
2021-06-16 22:56:48 -0700

    added env file and git ignore installed dependencies and started filling in empty functions

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -1,11 +1,27 @@
 const router = require('express').Router();
-const { Category, Product } = require('../../models');
+const { Category, Product } = require('../../models/Index');
 
 // The `/api/categories` endpoint
 
 router.get('/', (req, res) => {
   // find all categories
   // be sure to include its associated Products
+
+  console.log('======================');
+  Category.findAll({
+    attributes: [
+      'id',
+      'category_name',
+    ],
+    include: [
+      // include the Comment model here:
+      {
+        model: Product,
+        attributes: ['id', 'category_name', ],
+        
+      },
+    ]
+  })
 });
 
 router.get('/:id', (req, res) => {
--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -1,24 +1,40 @@
 const router = require('express').Router();
-const { Tag, Product, ProductTag } = require('../../models');
+const { Tag, Product, ProductTag, Category } = require('../../models/Index');
 
 // The `/api/tags` endpoint
 
 router.get('/', (req, res) => {
   // find all tags
   // be sure to include its associated Product data
+  console.log('======================');
+  Category.findAll({
+
+  })
 });
 
 router.get('/:id', (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
+  console.log('======================');
+  Category.findOne({
+    
+  })
 });
 
 router.post('/', (req, res) => {
   // create a new tag
+
+  Category.create({
+    
+  })
 });
 
 router.put('/:id', (req, res) => {
   // update a tag's name by its `id` value
+
+  Category.update({
+    
+  })
 });
 
 router.delete('/:id', (req, res) => {
